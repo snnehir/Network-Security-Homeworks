6e596f37deaef3a9a263716e858912355c9d8088
mint <snnehir21@gmail.com>
2022-03-15 19:08:36 +0300

    Image encryption using CBC mode

---
+++ cbc_mode/cbc.py
@@ -0,0 +1,50 @@
+import numpy as np
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad, unpad
+import cv2
+
+
+BLOCK_SIZE = 16 
+IMG_SIZE = 300
+IMG_SIZE_ROUND = IMG_SIZE + (BLOCK_SIZE - (IMG_SIZE% BLOCK_SIZE))
+
+
+def encrypt_img(plain_img, key, iv):
+    L2 = []
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted_img= np.array(range(IMG_SIZE_ROUND*IMG_SIZE_ROUND), int).reshape((IMG_SIZE_ROUND, IMG_SIZE_ROUND))
+    for i in range(IMG_SIZE):
+        img_bytes= bytes(plain_img[i,:].tolist())
+        msg =  cipher.encrypt(pad(img_bytes, BLOCK_SIZE))
+        for p in msg:
+            L2 += [(p)]
+        encrypted_img[i,:]=L2[:]
+        L2=[]
+    cv2.imwrite('cbc_mode/encrypted.png', encrypted_img)
+    return encrypted_img
+
+
+
+def decrypt_img(encrypted_img, key, iv):
+    L2=[]
+    decrypted_img= np.array(range(IMG_SIZE*IMG_SIZE), int).reshape((IMG_SIZE, IMG_SIZE))
+    plain = AES.new(key, AES.MODE_CBC, iv)
+    for i in range(IMG_SIZE):
+        img_bytes=bytes(encrypted_img[i,:].tolist())
+        # decrypt
+        msg = unpad(plain.decrypt(img_bytes), 16)
+        for p in msg:
+            L2 += [(p)]
+        decrypted_img[i,:]=L2[:]
+        L2=[]
+    cv2.imwrite('cbc_mode/decrypted.png', decrypted_img)
+
+
+plain_img = cv2.imread('turtle.png', 0)   # read image 3D
+
+key = b'my very secret k' # 16-byte key (random may be used)
+iv = b'0000000000000000' # initialization vector
+
+dec_img = encrypt_img(plain_img, key, iv)
+
+decrypt_img(dec_img, key, iv)
