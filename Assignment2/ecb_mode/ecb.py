ebf915619cd419bd780c615400d768b6cd66ca81
mint <snnehir21@gmail.com>
2022-03-17 21:32:21 +0300

    New directory.

--- Assignment2/ecb_mode/ecb.py
+++ Assignment2/ecb_mode/ecb.py
@@ -4,29 +4,35 @@ from Cryptodome.Util.Padding import pad, unpad
 import cv2 # opencv for using read/write image operations
 
 BLOCK_SIZE = 16 
-IMG_SIZE = 300
-IMG_SIZE_ROUND = IMG_SIZE + (BLOCK_SIZE - (IMG_SIZE% BLOCK_SIZE))
 
 
-def encrypt_img(plain_img, key):
-    L2 = []
+
+
+def encrypt_img(plain_img, key, IMG_SIZE = 300):
+    col = []
+    # Block ciphers in ECB or CBC mode require their input to be an exact multiple of the block length. 
+    # Any odd bytes need to be padded to the next multiple.
+    IMG_SIZE_ROUND = IMG_SIZE + (BLOCK_SIZE - (IMG_SIZE% BLOCK_SIZE))
+
     cipher = AES.new(key, AES.MODE_ECB)
+
     encrypted_img= np.array(range(IMG_SIZE_ROUND * IMG_SIZE_ROUND), int).reshape((IMG_SIZE_ROUND, IMG_SIZE_ROUND))
+
     for i in range(IMG_SIZE):
         img_bytes= bytes(plain_img[i,:].tolist())
         # encrypt
         msg =  cipher.encrypt(pad(img_bytes, BLOCK_SIZE))
         for p in msg:
-            L2 += [(p)] # stores encrypted bytes in the row
-        encrypted_img[i,:]=L2[:]    # stores the entire encrpyted image
-        L2=[]
+            col += [(p)] # stores encrypted bytes in the row
+        encrypted_img[i,:]=col[:]    # stores the entire encrpyted image
+        col = []
     cv2.imwrite('ecb_mode/encrypted.png', encrypted_img)
     
     return encrypted_img
 
 
-def decrypt_img(encrypted_img, key):
-    L2=[]
+def decrypt_img(encrypted_img, key, IMG_SIZE = 300):
+    col=[]
     decrypted_img= np.array(range(IMG_SIZE*IMG_SIZE), int).reshape((IMG_SIZE,IMG_SIZE))
     plain = AES.new(key, AES.MODE_ECB)
     for i in range(IMG_SIZE):
@@ -34,9 +40,9 @@ def decrypt_img(encrypted_img, key):
         # decrypt
         msg = unpad(plain.decrypt(img_bytes), BLOCK_SIZE)
         for p in msg:
-            L2 += [(p)] # stores decrypted bytes in the row
-        decrypted_img[i,:]=L2[:]    # stores the entire decrpyted image
-        L2=[]
+            col += [(p)] # stores decrypted bytes in the row
+        decrypted_img[i,:]=col[:]    # stores the entire decrpyted image
+        col=[]
     cv2.imwrite('ecb_mode/decrypted.png', decrypted_img)
     
 
