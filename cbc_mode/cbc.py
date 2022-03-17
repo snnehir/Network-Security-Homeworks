0b2cad5f075ec44813cb0e0d0ed11b30caf4b9d5
mint <snnehir21@gmail.com>
2022-03-17 21:25:01 +0300

    CBC mode IV updated.

--- cbc_mode/cbc.py
+++ cbc_mode/cbc.py
@@ -1,32 +1,38 @@
 import numpy as np
+from Cryptodome import Random
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
 import cv2
 
 
 BLOCK_SIZE = 16 
-IMG_SIZE = 300
-IMG_SIZE_ROUND = IMG_SIZE + (BLOCK_SIZE - (IMG_SIZE% BLOCK_SIZE))
 
 
-def encrypt_img(plain_img, key, iv):
-    L2 = []
+
+
+def encrypt_img(plain_img, key, iv, IMG_SIZE = 300):
+    col = []
+    # Block ciphers in ECB or CBC mode require their input to be an exact multiple of the block length. 
+    # Any odd bytes need to be padded to the next multiple.
+    IMG_SIZE_ROUND = IMG_SIZE + (BLOCK_SIZE - (IMG_SIZE% BLOCK_SIZE))
     cipher = AES.new(key, AES.MODE_CBC, iv)
     encrypted_img= np.array(range(IMG_SIZE_ROUND*IMG_SIZE_ROUND), int).reshape((IMG_SIZE_ROUND, IMG_SIZE_ROUND))
+
     for i in range(IMG_SIZE):
         img_bytes= bytes(plain_img[i,:].tolist())
+        # encrypt 
         msg =  cipher.encrypt(pad(img_bytes, BLOCK_SIZE))
         for p in msg:
-            L2 += [(p)]
-        encrypted_img[i,:]=L2[:]
-        L2=[]
+            col += [(p)]
+        encrypted_img[i,:]=col[:]
+        col=[] # clear list
     cv2.imwrite('cbc_mode/encrypted.png', encrypted_img)
     return encrypted_img
 
 
 
-def decrypt_img(encrypted_img, key, iv):
-    L2=[]
+def decrypt_img(encrypted_img, key, iv, IMG_SIZE = 300):
+    col = []
     decrypted_img= np.array(range(IMG_SIZE*IMG_SIZE), int).reshape((IMG_SIZE, IMG_SIZE))
     plain = AES.new(key, AES.MODE_CBC, iv)
     for i in range(IMG_SIZE):
@@ -34,16 +40,16 @@ def decrypt_img(encrypted_img, key, iv):
         # decrypt
         msg = unpad(plain.decrypt(img_bytes), 16)
         for p in msg:
-            L2 += [(p)]
-        decrypted_img[i,:]=L2[:]
-        L2=[]
+            col += [(p)]
+        decrypted_img[i,:]=col[:]
+        col=[] # clear list
     cv2.imwrite('cbc_mode/decrypted.png', decrypted_img)
 
 
 plain_img = cv2.imread('turtle.png', 0)   # read image (300x300)
 
 key = b'my very secret k' # 16-byte key
-iv = b'0000000000000000' # initialization vector
+iv = Random.new().read(BLOCK_SIZE) # initialization vector should be randomly selected
 
 dec_img = encrypt_img(plain_img, key, iv)
 
