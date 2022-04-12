a8bc86d3019de940f825ed722797112da7667e77
mint <snnehir21@gmail.com>
2022-04-13 02:40:38 +0300

    all cases are completed

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,89 +1,145 @@
-# generate password file that is contain 6-digit numbers only (from 000000 to 999999)
+# password format: 6-digit numbers only (from 000000 to 999999)
 import hashlib
 import random
 import time
 
-passwords = []
-number_of_users = 32
-attackers_dictionary = []
-dictionary_size = 1024
-salt_key = "saltkey123"  # should be randomly generated
+
+NUMBER_OF_USERS = 32
+SALT = "$2b$12$04kvO35Qm7nEPTGFSSI43e"  # constant non-secret value
 
 
-def get_salted_hashed_password(plain_password, salt_key):
+# helper function to convert plain password into hashed password
+def get_hashed_salted_password(plain_password):
     pw = str(plain_password).zfill(6)   # 123 -> 000123 (6 digits)
-    salted_pw = bytes(pw + salt_key, "utf-8")
+    salted_pw = bytes(pw + SALT, "utf-8")
     hashed_pw = hashlib.md5(salted_pw).hexdigest()
     return hashed_pw
 
 
-# construct attacker's dictionary (1024)
+# construct attacker's dictionary (finding matching passwords in Case4 was so hard when passwords ind dictionary are generated randomly.)
 # suppose Alice's password is in the attacker's dictionary
-def create_dictionary(dictionary_size, alices_password=None):
+def create_dictionary(dictionary_size, alices_plain_password=None):
     attackers_dictionary = []
 
-    if alices_password is not None:
+    if alices_plain_password is not None:
         dictionary_size -= 1
-        attackers_dictionary.append(alices_password)
+        attackers_dictionary.append(alices_plain_password)
 
     for i in range(dictionary_size):
-        pw = str(random.randint(0, 999999)).zfill(6)
-        attackers_dictionary.append(pw)
+        password = str(i).zfill(6)
+        # do not include same passwords
+        if password == alices_plain_password:
+            password = str(dictionary_size).zfill(6)
+        attackers_dictionary.append(password)
 
     random.shuffle(attackers_dictionary)
     return attackers_dictionary
 
 
-def create_users_password(number_of_users, salt_key):
+# create passwords for n users
+def create_users_password():
     passwords = []
     # choose random passwords
-    for i in range(number_of_users):
-        hashed_pw = get_salted_hashed_password(
-            random.randint(0, 999999), salt_key)
-        passwords.append(hashed_pw)
+    for i in range(NUMBER_OF_USERS):
+        # small range is used for finding matching passwords in Case4
+        # (otherwise it's almost infeasible to find matching passwords because attacker's dictionary size is too small)
+        hashed_password = get_hashed_salted_password(random.randint(0, 1500))
+        passwords.append(hashed_password)
+
     return passwords
 
 
-# brute forcing without using dictionary
-def case1_cracking(alices_password, salt_key):
+#   CASE FUNTIONS
+
+# brute forcing. attacker is not using her dictionary
+def case1_cracking(alices_password):
     start_time = time.time()
     for i in range(1000000):
-        hashed_pw = get_salted_hashed_password(i, salt_key)
+        hashed_pw = get_hashed_salted_password(i)
         if hashed_pw == alices_password:
             time_to_crack = time.time() - start_time
-            return f"[Case 1] Alice's password {i} is cracked in {time_to_crack} seconds."
+            print(
+                f"[Case 1] Alice's password {i} is cracked in {time_to_crack} seconds.")
 
 
 # attacker uses her dictionary
-def case2_cracking(alices_password, salt_key, dictionary_size, alices_plain_password):
+def case2_cracking(alices_password, dictionary_size, alices_plain_password):
+    is_found = False
     start_time = time.time()
-    attackers_dictionary = create_dictionary(
-        dictionary_size, alices_plain_password)
 
-    for i in range(dictionary_size):
-        hashed_pw = get_salted_hashed_password(
-            attackers_dictionary[i], salt_key)
+    # if you want to assume that Alice's password is in the dictionary, use this line and comment line 75:
+    # attackers_dictionary = create_dictionary(dictionary_size, alices_plain_password)
+
+    # if you do not want to assume Alice's password is in the dictionary, use this line instead of above line:
+    attackers_dictionary = create_dictionary(dictionary_size)
+
+    for pw in attackers_dictionary:
+        hashed_pw = get_hashed_salted_password(pw)
         if hashed_pw == alices_password:
             time_to_crack = time.time() - start_time
-            return f"[Case 2] Alice's password {attackers_dictionary[i]} is cracked in {time_to_crack} seconds."
+            print(
+                f"[Case 2] Alice's password {pw} is cracked in {time_to_crack} seconds.")
+            is_found = True
+            break
+
+    # if Alice's password is not in attacker's dictionary, use brute force
+    if not is_found:
+        print("Alice's password is not found in dictionary. Brute forcing...")
+        for i in range(1000000):
+            hashed_pw = get_hashed_salted_password(i)
+            if hashed_pw == alices_password:
+                time_to_crack = time.time() - start_time
+                print(
+                    f"[Case 2] Alice's password {i} is cracked in {time_to_crack} seconds.")
 
 
 def case3_cracking():
-    pass
+    passwords = create_users_password()
+    start_time = time.time()
+    for i in range(1000000):
+        hashed_pw = get_hashed_salted_password(i)
+        # looking each password for any matching password
+        if hashed_pw in passwords:
+            time_to_crack = time.time() - start_time
+            print(
+                f"[Case 3] Password {str(i).zfill(6)} is cracked in {time_to_crack} seconds.")
+            break
 
 
-def case4_cracking():
-    pass
+def case4_cracking(dictionary_size):
+    passwords = create_users_password()
+    start_time = time.time()
+    attackers_dictionary = create_dictionary(dictionary_size)
+    is_found = False
+
+    for attacker_pw in attackers_dictionary:
+        hashed_pw = get_hashed_salted_password(attacker_pw)
+        # looking each password for any matching password
+        if hashed_pw in passwords:
+            time_to_crack = time.time() - start_time
+            print(
+                f"[Case 4] Password {attacker_pw} is cracked in {time_to_crack} seconds.")
+            is_found = True
+            break
+
+    if not is_found:
+        print("[Case 4] No matching password found from dictionary")
+
+#  END OF CASE FUNCTIONS
 
 
-######################################### Main section #########################################
+alices_plain_password = "148513"
+alices_pw = get_hashed_salted_password(alices_plain_password)
+dictionary_size = 1024
 
-alices_plain_password = "923576"
-alices_pw = get_salted_hashed_password(alices_plain_password, salt_key)
-
-print("Alice's pw: ", alices_pw)
+print("\nSalt key used: ", SALT)
+print("\nAlice's password: ", alices_plain_password)
+case1_cracking(alices_pw)
 print()
-print(case1_cracking(alices_pw, salt_key))
-
-print("\n-------------------------\n")
-print(case2_cracking(alices_pw, salt_key, 1024, alices_plain_password))
+print("Alice's password: ", alices_plain_password)
+case2_cracking(alices_pw, dictionary_size, alices_plain_password)
+print()
+case3_cracking()
+print()
+case4_cracking(dictionary_size)
+print()
